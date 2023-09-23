e870c84140a55cfe21e12eb31587eeaf8ab61ad1
Takasur Azeem <takasurazeem@gmail.com>
2023-09-24 01:26:07 +0500

    Initial Commit

---
+++ ListWithSelections/ListWithSelectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListWithSelectionsApp.swift
+//  ListWithSelections
+//
+//  Created by Takasur Azeem on 24/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ListWithSelectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
